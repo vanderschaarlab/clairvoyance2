1e939c99a970b07e1c7c4334d460640c02a33720
Bogdan Cebere <bogdan.cebere@gmail.com>
2023-03-18 07:14:04 +0000

    debug

--- src/clairvoyance2/data/df_constraints.py
+++ src/clairvoyance2/data/df_constraints.py
@@ -108,6 +108,8 @@ class ConstraintsChecker:
     @staticmethod
     def _check_elements(df: pd.DataFrame, constraints: ElementConstraints) -> None:
         if constraints.dtypes is not None and len(constraints.dtypes) > 0:
+            print("expected", constraints.dtypes)
+            print("actual", df.dtypes.values)
             if not all(dtype in constraints.dtypes for dtype in df.dtypes.values):
                 raise TypeError(
                     f"DataFrame elements must be limited to dtypes: {constraints.dtypes}. "
